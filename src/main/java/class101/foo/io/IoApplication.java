01aa9b231bb6af06832285b97619dc76d758f4d1
KingBlackCow <sgs1159@naver.com>
2022-04-06 00:58:22 +0900

    feat: 첫 페이지 캐싱

--- src/main/java/class101/foo/io/IoApplication.java
+++ src/main/java/class101/foo/io/IoApplication.java
@@ -2,7 +2,9 @@ package class101.foo.io;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling// 크론표현식을 활용해 스케줄링가능
 @SpringBootApplication
 public class IoApplication {
 
